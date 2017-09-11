ca0b6dc260a8505eaa56bd01584e48ad8a994923
Moralesc1427 <31833665+Moralesc1427@users.noreply.github.com>
2017-09-11 19:19:41 -0400

    Add files via upload

---
+++ M2HW1_DistanceTraveled_Morales.py
@@ -0,0 +1,14 @@
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:14:34) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+# CTI-110
+# M2HW1 - Distance Traveled
+# Carlos Morales
+# 09/10/2017
+speed = 70
+distanceAfter6 = speed * 6
+distanceAfter10 = speed * 10
+distanceAfter15 = speed * 15
+print("After 6 hours, the vehicle will travel ",distanceAfter6,"miles.")
+print("After 10 hours, the vehicle will travel ",distanceAfter10,"miles.")
+print("After 15 hours, the vehicle will travel ",distanceAfter15,"miles.")
